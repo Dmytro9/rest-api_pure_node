9ce0da612c76412e3d9fa2e02c755846030b3080
Dmytro9 <dpoliuliuk@gmail.com>
2018-09-10 23:49:15 +0300

    request object

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 var http = require('http');
+var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
-var server = http.createServer(function (req, res) {
-  res.end('Hello World\n');
+
+var server = http.createServer(function(req, res) {
+    // parse url
+    var parseUrl = url.parse(req.url, true);
+
+    // get the path
+    var path = parseUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // get query string
+    var queryStringObject = parseUrl.query;
+
+    // get the headers
+    var headers = req.headers;
+
+    // get http method
+    var method = req.method.toLocaleLowerCase();
+
+    // get the payload, if any
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', function(data) {
+        buffer += decoder.write(data);
+    });
+    req.on('end', function() {
+        buffer += decoder.end();
+
+        // send the response
+        res.end('Hello World\n');
+
+        // log the request path
+        console.log('payload', buffer);
+    });
+
 });
 
 
-server.listen(3000, function () {
-  console.log('listening on 3000...');
+server.listen(3000, function() {
+    console.log('listening on 3000...');
 });
