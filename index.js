e24b61c7c41e3b13e42a971206520d103932ec1c
Dmytro9 <dpoliuliuk@gmail.com>
2018-09-10 22:43:40 +0300

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+  res.end('Hello World\n');
+});
+
+
+server.listen(3000, function () {
+  console.log('listening on 3000...');
+});
