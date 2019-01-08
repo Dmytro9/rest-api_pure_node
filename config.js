fc3b025cf84fdde23e98742108732b481fb49306
Dmytro9 <dpoliuliuk@gmail.com>
2019-01-09 00:04:33 +0200

    finish check-post

--- config.js
+++ config.js
@@ -12,6 +12,7 @@ environments.staging = {
   'httpsPort': 3001,
   'envName': 'staging',
   'hashingSecret': 'thisIsASecret',
+  'maxChecks': 5,
 }
 
 // production environment
@@ -20,6 +21,7 @@ environments.production = {
   'httpsPort': 5001,
   'envName': 'production',
   'hashingSecret': 'thisIsAlsoASecret',
+  'maxChecks': 5,
 }
 
 // determine which env was passed as a command-line argment
--- index.js
+++ index.js
@@ -96,5 +96,7 @@ var unifiedServer = function (req, res) {
 var router = {
 	'ping': handlers.ping,
 	'users': handlers.users,
-	'tokens': handlers.tokens
+	'tokens': handlers.tokens,
+	'checks': handlers.checks,
+	'notFound': handlers.notFound,
 }
