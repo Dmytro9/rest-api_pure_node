7b1d2903332c594dab5a898de6064a3fc7d36820
Dmytro9 <dpoliuliuk@gmail.com>
2019-01-21 22:38:54 +0200

    refactoring and create worker fn

--- index.js
+++ index.js
@@ -5,4 +5,21 @@
 
 // Dependencies
 var server = require('./lib/server');
+var workers = require('./lib/workers');
 
+// Declare the app
+var app = {};
+
+// Init function
+app.init = function () {
+	// Start the server
+	server.init();
+	// Start the workers
+	// workers.init();
+};
+
+// Execute
+app.init();
+
+// Export the app
+module.export = app;
