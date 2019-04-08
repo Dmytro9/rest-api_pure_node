4d68367a07a7615b3688346c7a60f66055d8cc79
Dmytro9 <dpoliuliuk@gmail.com>
2019-04-08 22:53:55 +0300

    strict mode - to start: node --use_strict index.js

---
+++ index-strict.js
@@ -0,0 +1,32 @@
+/*
+* Primary file to the API
+*
+**/
+
+// Dependencies
+var server = require('./lib/server');
+var workers = require('./lib/workers');
+var cli = require('./lib/cli');
+
+// Declare the app
+app = {};
+
+// Init function
+app.init = function () {
+	// Start the server
+	server.init();
+
+	// Start the workers
+	// workers.init();
+
+	// STart the CLI (starts last)
+	setTimeout(function () {
+		cli.init();
+	}, 500);
+};
+
+// Execute
+app.init();
+
+// Export the app
+module.export = app;
