77a80992fff9c6e752e32d7b0ca5f0e6cbbec87f
Dmytro9 <dpoliuliuk@gmail.com>
2019-05-09 16:48:41 +0300

    http2

---
+++ misc/http2-client.js
@@ -0,0 +1,32 @@
+/*
+ * Example HTTP2 Client
+ *
+ */
+
+// Dependencies
+var http2 = require('http2');
+
+// Create Client
+var client = http2.connect('http://localhost:7000');
+
+
+// Create the request
+var req = client.request({
+    ':path': '/'
+});
+
+// When a message (respond) is received, add the pieces of it together until you reach the end
+var str = '';
+req.on('data', function (chunk) {
+    str += chunk;
+});
+
+
+
+// When the message ends, log it out
+req.on('end', function(){
+    console.log(str);
+});
+
+// End the request
+req.end();
